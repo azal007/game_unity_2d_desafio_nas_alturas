e9d80526bb365265c222e627ab450b5c777de465
Ari <arizaluar@gmail.com>
2020-05-09 18:56:23 -0300

    Scene created, with the plane and the landscape, the obstacles.

---
+++ Assets/Scripts/Obstaculo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaculo : MonoBehaviour
+{
+    [SerializeField]
+    private float velocidade = 0.5f;
+
+    // Update is called once per frame
+    void Update() {
+        this.transform.Translate(Vector3.left * this.velocidade * Time.deltaTime);
+    }
+}
